f8bc17a7246e57a51414b8a695c17e06ebddb993
Kelly Meier <kemeier2000@yahoo.com>
2019-07-28 16:04:28 -0500

    added final page

---
+++ assets/javascript/app.js
@@ -0,0 +1,32 @@
+$(document).ready(function ()   {
+
+var correct = 0;
+var incorrect = 0;
+var unanswered = 0;
+
+
+$("#correct").text(correct);
+$("#losses").text(correct);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
