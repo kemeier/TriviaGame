7d5235ecc1341a548f25b8eea937f2b4c2ed2cd7
Kelly Meier <kemeier2000@yahoo.com>
2019-07-28 18:58:21 -0500

    working timer on page

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -8,19 +8,25 @@ var timeRemaining = 0;
 
 var timer = 21; 
 
-a = setInterval(function(){ 
+timeRemaining = setInterval(function(){ 
 
 timer = timer-1;
 
 if(timer == 0){ 
-    clearInterval(a);
+    clearInterval(timeRemaining);
  } 
 console.log(timer);
+$("#timed").html(timer);
 }, 1000);
+
+   
+
 
 
 $("#start-button").click( function(){
+    timeRemaining();
 
+ 
  });
 
 
@@ -28,7 +34,7 @@ $("#start-button").click( function(){
 $("#correct-answers").text("Correct Answers: " + correct);
 $("#incorrect-answers").text("Incorrect Answers: " + incorrect);
 $("#unanswered-questions").text("Unanswered: " + unanswered);
-$("#time-remaining").text("Time Remaining: ") + timeRemaining;
+$("#timed").text("Time Remaining: ") + timer;
 
 
 });
