63130d5af8e86a363303ffef7b46d33d21dc6ce1
Kelly Meier <kemeier2000@yahoo.com>
2019-07-29 10:43:00 -0500

    updated

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -19,16 +19,9 @@ console.log(timer);
 $("#timed").html(timer);
 }, 1000);
 
-   
-
-
-
 $("#start-button").click( function(){
     timeRemaining();
-
- 
  });
-
 
 
 $("#correct-answers").text("Correct Answers: " + correct);
@@ -40,21 +33,3 @@ $("#timed").text("Time Remaining: ") + timer;
 });
 
 
-function startTimer(){
-    var counter = 5;
-    setInterval(function() {
-      counter--;
-      if (counter >= 0) {
-        span = document.getElementById("count");
-        span.innerHTML = counter;
-      }
-      if (counter === 0) {
-          alert('sorry, out of time');
-          clearInterval(counter);
-      }
-    }, 1000);
-  }
-  
-  $("#startClock").click(function(){
-      startTimer();
-  });
