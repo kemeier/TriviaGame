b0e9d86d8721b627f142680f7db77b1675a6c336
Kelly Meier <kemeier2000@yahoo.com>
2019-07-30 22:59:03 -0500

    connected scoring

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # TriviaGame
+Game in which the user presses "start" and is timed down from 20 seconds while choosing the correct answers to three different questions.  If the user finishes before the timer ends, they can press "done" and their score will be shown.
+
+I used html, bootstrap, css, javascript and jquery to create this game.
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,77 +1,95 @@
-$(document).ready(function ()   {
+$(document).ready(function () {
 
-var correct = 0;
-var incorrect = 0;
-var unanswered = 0;
-var timeRemaining = 0;
+    var correct = 0;
+    var incorrect = 0;
+    var unanswered = 0;
+    var timeRemaining = 0;
 
-
-var timer = 21; 
-
-timeRemaining = setInterval(function(){ 
-
-timer = timer-1;
-
-if(timer === 0){ 
-    clearInterval(timeRemaining);
- } 
-console.log(timer);
-$("#timed").html("Time Remaining: " + timer);
-}, 1000);
-
-$("#start-button").click( function(){
-    timeRemaining();
- });
-
- var questions =[{ 
-	question:"The heart of a shrimp is located in its:" ,
-	answers:["Foot", "Abdomen", "Head"],
-	correctAnswer:"Head"
-}, {
-	question:"The amount of time it takes for a sloth to digest its food is:" ,
-	answers:["2 days", "2 weeks", "2 years"],
-	correctAnswer:"2 weeks"
-}, {
-	question:"Nearly 3 percent of ice in Antarctic glaciers is made of" ,
-	answers:["Penguin urine", "Fresh water", "CO2 in dry ice form"],
-	correctAnswer:"Penguin urine"
-}];
+    var timeRemaining;
+    var timer = 20;
 
 
-$.each($('input[name="question-1"]:checked'), function(){ 
-    if($(this).val()=== questions[0].correctAnswer){
-        correct++;
-    } else { 
-        incorrect++;
+
+    $("#start-button").click(function () {
+        console.log("here");
+        $("#triviapage").removeClass("d-none");
+        $("#welcome-page").addClass("d-none");
+        timeRemaining = setInterval(function () {
+
+            timer = timer - 1;
+
+            if (timer === 0) {
+                clearInterval(timeRemaining);
+                $("#triviapage").addClass("d-none");
+                $("#finishpage").removeClass("d-none");
+                scoring();
+            }
+
+            console.log(timer);
+            $("#timed").html("Time Remaining: " + timer);
+        }, 1000);
+    });
+
+    $("#done").click(function (event) {
+        event.preventDefault();
+        clearInterval(timeRemaining);
+        $("#triviapage").addClass("d-none");
+        $("#finishpage").removeClass("d-none");
+        scoring();
+
+    });
+
+    var questions = [{
+        question: "The heart of a shrimp is located in its:",
+        answers: ["Foot", "Abdomen", "Head"],
+        correctAnswer: "Head"
+    }, {
+        question: "The amount of time it takes for a sloth to digest its food is:",
+        answers: ["2 days", "2 weeks", "2 years"],
+        correctAnswer: "2 weeks"
+    }, {
+        question: "Nearly 3 percent of ice in Antarctic glaciers is made of",
+        answers: ["Penguin urine", "Fresh water", "CO2 in dry ice form"],
+        correctAnswer: "Penguin urine"
+    }];
+
+    function scoring() {
+        var answer1= $('input[name="q1"]:checked').val()
+        var answer2= $('input[name="q2"]:checked').val()
+        var answer3= $('input[name="q3"]:checked').val()
+        console.log(answer2);
+        console.log(questions[0].correctAnswer)
+        if ( answer1 === questions[0].correctAnswer) {
+            correct++;
+        } else {
+            incorrect++;
+        }
+
+        if (answer2 === questions[1].correctAnswer) {
+            correct++;
+        } else {
+            incorrect++;
+        }
+
+
+        if (answer3 === questions[2].correctAnswer) {
+            correct++;
+        } else {
+            incorrect++;
+        }
+
+        $("#correct-answers").text("Correct Answers: " + correct);
+        $("#incorrect-answers").text("Incorrect Answers: " + incorrect);
     }
-    console.log(this.value);
-});
-
-$.each($('input[name="question-2"]:checked'), function(){ 
-    if($(this).val()=== questions[1].correctAnswer){
-        correct++;
-    } else { 
-        incorrect++;
-    }
-    console.log(this.value);
-});
-
-
-$.each($('input[name="question-3"]:checked'), function(){ 
-    if($(this).val()=== questions[2].correctAnswer){
-        correct++;
-    } else { 
-        incorrect++;
-    }
-    console.log(this.value);
-});
 
 
 
-$("#correct-answers").text("Correct Answers: " + correct);
-$("#incorrect-answers").text("Incorrect Answers: " + incorrect);
-$("#unanswered-questions").text("Unanswered: " + unanswered);
-$("#timed").text("Time Remaining: " + timer);
+    $("#unanswered-questions").text("Unanswered: " + unanswered);
+    $("#timed").text("Time Remaining: " + timer);
+
+    setTimeout(function () {
+        $("#timed").text("Time Remaining: " + timer);
+    }, 1000);
 
 
 
